be4ac9eb98da03a637b9836ef95ff53ddb577585
raharjomukti <raharjomukti09@gmail.com>
2026-01-13 01:26:10 +0700

    Live Code Up new S-3

--- app/(landing)/components/payment/payment-option.tsx
+++ app/(landing)/components/payment/payment-option.tsx
@@ -1,5 +1,6 @@
 import { FiCreditCard } from "react-icons/fi";
 import CardWithHeader from "../ui/card-with-header";
+import { getAllBanks } from "@/app/services/bank.service";
 
 const paymentList = [
   {
@@ -34,28 +35,28 @@ const paymentList = [
   }
 ];
 
-const PaymentOption = () => {
-  return (
-    <div>
-        <CardWithHeader title="Payment Option">
-            {paymentList.map((payment, index) => (
-                <div className="flex gap-5 p-4.5 border-b border-gray-100" key={index}>
-                    <div className="bg-blue-100 p-4 text-blue-500 h-fit self-center rounded-md">
-                        <FiCreditCard size={25} />
-                    </div>
-                    <div className="self-center">
-                        <div className="font-bold">{payment.bank_name}</div>
-                        <div className="text-sm">{payment.account_number}</div>
-                        <div className="text-sm opacity-70">{payment.account_holder}</div>
-                    </div>
-                    <div className="ml-auto bg-blue-50 text-gray-800 text-xs h-fit self-center px-3 py-1 rounded-full">
-                        Bank Transfer
-                    </div>
-                </div>
-            ))}
-        </CardWithHeader>
-    </div>
-);
-}
+const PaymentOptions = async () => {
+  const banks = await getAllBanks();
 
-export default PaymentOption; 
+  return (
+    <CardWithHeader title="Payment Options">
+      {banks.map((payment, index) => (
+        <div className="flex gap-5 p-5 border-b border-gray-100" key={index}>
+          <div className="bg-blue-100 p-4 text-blue-500 h-fit self-center">
+            <FiCreditCard size={24} />
+          </div>
+          <div className="self-center">
+            <div className="font-bold">{payment.bankName}</div>
+            <div className="text-sm">{payment.accountNumber}</div>
+            <div className="text-sm opacity-70">{payment.accountName}</div>
+          </div>
+          <div className="ml-auto bg-blue-50 text-gray-800 text-xs h-fit self-center px-2 py-1">
+            Bank Transfer
+          </div>
+        </div>
+      ))}
+    </CardWithHeader>
+  );
+};
+
+export default PaymentOptions;
--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -1,25 +1,26 @@
+"use client";
+
 import OrderConfirmed from "../../components/order-status/order-confirmed";
-import OrderSubmitted from "../../components/order-status/order-submitted";
+import OrderSubmited from "../../components/order-status/order-submited";
 import { getTransactionById } from "@/app/services/transaction.service";
 import { TPageProps } from "../../product/[id]/page";
-import OrderRejected from "../../components/order-status/order-rejected";
 
-const OrderStatus = async ({ params }: TPageProps) => {
-  const { id } = await params;
+const OrderStatus = async ({params}: TPageProps) => {
+  const {id} = await params
 
-  const transaction = await getTransactionById(id);
-  console.log("transaction", transaction);
+  const transaction = await getTransactionById(id)
+  console.log("Trancation", transaction);
 
   return (
     <main className="bg-gray-100 min-h-[80vh]">
-      <div className="max-w-5xl mx-auto pt-40 pb-20">
-        <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
+      <div className="max-w-5xl mx-auto py-20">
+        <h1 className="text-5xl font-bold text-center mb-11">
+          Order Status
+        </h1>
       </div>
-      {transaction.status === "pending" && <OrderSubmitted />}
-      {transaction.status === "paid" && <OrderConfirmed />}
-      {transaction.status === "rejected" && <OrderRejected />}
+      {transaction.status === "paid" ?  <OrderConfirmed /> : <OrderSubmited />}
     </main>
   );
-};
+}
 
 export default OrderStatus;
